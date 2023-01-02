710459a5db2ae2cd875255af51ce0fc78f49d4f9
Harry Zhang <xiaojun.zhang@thoughtworks.com>
2023-01-02 22:18:57 +0800

    update script

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,6 +22,11 @@ which wget
 which brew
 
 which node
+
+brew install pipx
+pipx ensurepath
+pipx install cumulusci
+
 # wget -qO- https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz | tar xJ -C sfdx --strip-components 1
 # ./sfdx/install
 # sfdx --version
