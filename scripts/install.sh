0538f918c0db85333867c6542642701ae9ab6c12
Harry Zhang <xiaojun.zhang@thoughtworks.com>
2023-01-02 22:35:07 +0800

    run cci flow.

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,24 +15,14 @@ env
 
 echo "+++ :hammer: Example tests"
 
-mkdir sfdx
 
-which wget
-
-which brew
-
-which node
-
-which cci
-
-which sfdx
 
 echo '--- cli version'
 # cci version
 
-echo '--sfdx version' 
-sfdx --version
-sfdx force:org:list
+echo '--- run cci flow' 
+
+cci flow run ci_feature --org dev
 
 # wget -qO- https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz | tar xJ -C sfdx --strip-components 1
 # ./sfdx/install
